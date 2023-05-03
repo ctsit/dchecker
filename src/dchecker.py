822ead02a59eafda13c4a820bf8ac5cd43eff627
Michael Bentz <mbentz@ufl.edu>
2023-05-03 16:38:43 -0400

    Bump release to 2.2.0

--- src/dchecker.py
+++ src/dchecker.py
@@ -75,7 +75,7 @@ DEFAULT_PREFIXES = """
     PREFIX core:    <http://vivoweb.org/ontology/core#>
 """
 
-VERSION = "2.1.2"
+VERSION = "2.2.0"
 
 XSD_INTEGER = "http://www.w3.org/2001/XMLSchema#integer"
 
